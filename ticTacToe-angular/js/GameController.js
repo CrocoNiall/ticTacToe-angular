8bdd971ae1821848302438228abeaef4a4a010dd
Niall Wallace <nj.wallace@outlook.com>
2015-10-12 20:10:15 +0100

    added player count functionality

--- ticTacToe-angular/js/GameController.js
+++ ticTacToe-angular/js/GameController.js
@@ -4,13 +4,15 @@ angular.module('ticTacToeApp')
 function GameController(){
 
   this.currentPlayer = 'x'
-  this.xMoves = []
-  this.oMoves = []
+  this.xMoves = [];
+  this.oMoves = [];
+  this.xWinCount = 0;
+  this.oWinCount = 0;
   
   this.board = [
-    [ { value: '', index: 1 }, { value: '', index: 2 }, { value: '', index: 3 } ],
-    [ { value: '', index: 4 }, { value: '', index: 5 }, { value: '', index: 6 } ],
-    [ { value: '', index: 7 }, { value: '', index: 8 }, { value: '', index: 9 } ]
+    [ { value: '', index: 0 }, { value: '', index: 1 }, { value: '', index: 2 } ],
+    [ { value: '', index: 3 }, { value: '', index: 4 }, { value: '', index: 5 } ],
+    [ { value: '', index: 6 }, { value: '', index: 7 }, { value: '', index: 8 } ]
     ];
 
 
@@ -24,17 +26,58 @@ function GameController(){
     if(cell.value === ''){
       cell.value = this.currentPlayer
       if (this.currentPlayer === 'x'){ 
-        this.xMoves.push(cell.index) }
+        this.xMoves.push(cell.index) 
+        this.checkWinner(this.xMoves, 'x')
+      }
       else if (this.currentPlayer === 'o'){ 
-        this.xMoves.push(cell.index) }
-      
+        this.xMoves.push(cell.index) 
+        this.checkWinner(this.oMoves, 'o')
+      }
+
       this.changePlayer(this.currentPlayer)
     }
   }
 
   this.changePlayer = function(current){
     current === 'x' ? this.currentPlayer = 'o' : this.currentPlayer = 'x'
-    debugger
+
   }
+
+  this.checkWinner = function(playerArray, player){
+
+    for (i = 0; i < this.winningCombinations.length; i++) {
+      winCounter = 0;
+
+      for (var j = 0; j < this.winningCombinations[i].length; j++) {
+        if(playerArray.indexOf(this.winningCombinations[i][j]) !== -1){
+          winCounter++;
+        }
+
+        if(winCounter === 3){
+          alert("Game over, " + player + " wins!");
+          this.incramentPlayerCount(player)
+        }
+      }
+    }
+  }
+
+  this.incramentPlayerCount = function(player) {
+    player === 'x' ? this.xWinCount++ : this.oWinCount++
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

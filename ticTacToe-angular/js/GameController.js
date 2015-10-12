8c4308c7cc9c295b0b0a0ceb77b5680ecc535e7c
Niall Wallace <nj.wallace@outlook.com>
2015-10-12 19:17:52 +0100

    Setting up game basics

--- ticTacToe-angular/js/GameController.js
+++ ticTacToe-angular/js/GameController.js
@@ -2,10 +2,21 @@ angular.module('ticTacToeApp')
   .controller('GameController', GameController)
 
 function GameController(){
-  this.board = [];
+  
+
+
+  this.board = [
+    [ { value: '-', index: 1}, { value: '-', index: 2}, { value: '-', index: 3 } ],
+    [ { value: '-', index: 4 }, { value: '-', index: 5 }, { value: '-', index: 6 } ],
+    [ { value: '-', index: 7 }, { value: '-', index: 8 }, { value: '-', index: 9 } ]
+    ];
+
+
   this.winningCombinations = [ [0,1,2],[3,4,5],
                                [6,7,8],[0,3,6], 
                                [1,4,7],[2,5,8],
                                [0,4,8],[2,4,6] ];
   this.currentPlayer = 'x';
+
+
 }

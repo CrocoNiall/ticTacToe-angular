fbc78a96c48c6d522d9372f7b45e84f7d84becc0
Niall Wallace <nj.wallace@outlook.com>
2015-10-12 18:45:50 +0100

    set up environment

---
+++ ticTacToe-angular/js/GameController.js
@@ -0,0 +1,11 @@
+angular.module('ticTacToeApp')
+  .controller('GameController', GameController)
+
+function GameController(){
+  this.board = [];
+  this.winningCombinations = [ [0,1,2],[3,4,5],
+                               [6,7,8],[0,3,6], 
+                               [1,4,7],[2,5,8],
+                               [0,4,8],[2,4,6] ];
+  this.currentPlayer = 'x';
+}
